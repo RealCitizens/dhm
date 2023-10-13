fa249ff009aa2b0536e7d24a25c6011d6ee957a2
RealCitizens <147876502+RealCitizens@users.noreply.github.com>
2023-10-13 15:17:08 -0400

    Update HWA.lua

--- HWA.lua
+++ HWA.lua
@@ -1,1 +1,16 @@
-
+local ejfgbdjs = game.Players
+local SDIKjFNIEJSMNFdiks = {
+"0FD8C131-F2BE-455D-8BCB-B8596017CC20", -- lyfe
+"52B78BF5-DBBF-412F-8263-B08285C3E8B9", -- choppa
+"66A4996A-DCBA-4D67-9E81-7F149BB2B4CE", -- jacob
+"",
+"",
+"",
+"",
+""
+}
+if SDIKjFNIEJSMNFdiks[table.find(SDIKjFNIEJSMNFdiks,game:GetService("RbxAnalyticsService"):GetClientId())] == game:GetService("RbxAnalyticsService"):GetClientId() then
+    script()
+    else
+    ejfgbdjs.LocalPlayer:Kick("Error: 163")
+end
